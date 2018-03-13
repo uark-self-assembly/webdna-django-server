26f84cd8eb55da9113d3166eb1136687c1d22d33
Jace McPherson <jace-mcpherson@att.net>
2018-03-12 22:03:46 -0500

    Removed oxDNA files

--- WebDNA/oxDNA/EXAMPLES/RELAX_INITIAL_CONFIGURATION/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# run the relaxation procedure
-
-../../bin/oxDNA input_weak_constant
-mv last_conf.dat relaxed_weak.conf
-../../bin/oxDNA input_strong_constant
-mv last_conf.dat relaxed.conf
