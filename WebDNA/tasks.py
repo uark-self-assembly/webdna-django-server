145559476114ab89ed82a93144fe10e56fac00ac
David Darling <ddarling29@gmail.com>
2018-04-01 15:29:52 -0500

    Simulations can now be queued into Celery Server
    Basic simulation queuing, output logging will need to be improved

--- WebDNA/tasks.py
+++ WebDNA/tasks.py
@@ -1,9 +1,16 @@
 from __future__ import absolute_import
 from webdna_server.celery import app
+import subprocess
+import os
 
 
 # ==========TASKS==========
 # TODO: Define tasks here
+
+@app.task()
+def execute_sim(proj_id, path):
+    print("Received new execution for project: " + proj_id)
+    p = subprocess.Popen(["oxDNA", os.getcwd() + "/" + path + "/input.txt"], cwd=os.getcwd() + "/" + path)
 
 
 @app.task
