147599898178d076b1ecb5ee9da6abbb7c0917c7
David Darling <ddarling29@gmail.com>
2018-04-02 13:09:47 -0500

    Celery Task improvements

--- WebDNA/tasks.py
+++ WebDNA/tasks.py
@@ -1,21 +1,25 @@
 from __future__ import absolute_import
 from webdna_server.celery import app
+from WebDNA.models import *
 import subprocess
 import os
 
-
-# ==========TASKS==========
-# TODO: Define tasks here
+dictionaries = {}
 
 @app.task()
 def execute_sim(proj_id, path):
     print("Received new execution for project: " + proj_id)
-    log = open(file=path + "/" + "stdout.log", mode='a')
+    log = open(file=path + "/" + "stdout.log", mode='w')
+    p = subprocess.Popen(["oxDNA", "input.txt"], cwd=os.getcwd() + "/" + path, stdout=log)
 
-    p = subprocess.Popen(["oxDNA", os.getcwd() + "/" + path + "/input.txt"], cwd=os.getcwd() + "/" + path, stdout=log)
+    dictionaries[proj_id] = execute_sim.request.id
+
     p.wait()
     log.close()
-    
+    p = Project(id=proj_id, job_running=False)
+    p.save(update_fields=['job_running'])
+
+
 @app.task
 def test():
     print("Test task received from WebDNA server!")
